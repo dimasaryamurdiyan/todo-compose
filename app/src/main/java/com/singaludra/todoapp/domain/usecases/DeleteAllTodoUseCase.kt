e91acf0a54d3ff4e3bb30463c9ebccf5df2841dc
dmurdiyan <dmurdiyan@binar.co.id>
2023-07-31 01:20:45 +0700

    feat: setup DeleteAllTodoUseCase

---
+++ app/src/main/java/com/singaludra/todoapp/domain/usecases/DeleteAllTodoUseCase.kt
@@ -0,0 +1,15 @@
+package com.singaludra.todoapp.domain.usecases
+
+import com.singaludra.todoapp.domain.base.BaseUseCase
+import com.singaludra.todoapp.domain.exception.IErrorHandler
+import com.singaludra.todoapp.domain.repository.IToDoRepository
+import javax.inject.Inject
+
+class DeleteAllTodoUseCase @Inject constructor(
+    private val repository: IToDoRepository,
+    errorHandler: IErrorHandler
+): BaseUseCase<Unit, Unit>(errorHandler) {
+    override suspend fun execute(parameters: Unit) {
+        repository.deleteAllTodo()
+    }
+}
